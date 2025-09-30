065c990fc687e5f800988fd657ee2b58436df9c5
Anderson <anderson.reis.barros@gmail.com>
2025-09-30 10:26:35 -0300

    Segunda versão calculadora

--- calculadora.py
+++ calculadora.py
@@ -1,15 +1,30 @@
-v1 = int(input('Digite o valor 1:'))
-v2 = int(input('Digite o valor 2:'))
-op = int(input('''Digite a operação: 1 - soma
-				 2 - subtracao 
-				 3 - multiplicacao 
-				 4 - divisao'''))
+v1 = int(input('Digite o valor 1: '))
+v2 = int(input('Digite o valor 2: '))
+op = int(input('''Digite a operação:
+1 - soma
+2 - subtração
+3 - multiplicação
+4 - divisão
+'''))
 
 if op == 1:
-	print(v1+v2)
+    resultado = v1 + v2
+    print(f"Soma = {resultado}")
+
 elif op == 2:
-	print(v1-v2)
+    resultado = v1 - v2
+    print(f"Subtração = {resultado}")
+
 elif op == 3:
-	print(v1*v2)
+    resultado = v1 * v2
+    print(f"Multiplicação = {resultado}")
+
+elif op == 4:
+    if v2 == 0:
+        print('Operação não permitida. Denominador não pode ser igual a zero.')
+    else:
+        resultado = v1 / v2
+        print(f"Divisão = {resultado}")
+
 else:
-	print(v1/v2)
+    print('Opção inválida. Encerrando o programa.')
