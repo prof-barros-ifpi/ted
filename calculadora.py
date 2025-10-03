cc0d2bc72498c4e571aa8dc183fffdd7aa45994e
Prof.Barros <anderson.reis.barros@gmail.com>
2025-10-03 07:25:25 -0300

    Commit do OpalaSpace

--- calculadora.py
+++ calculadora.py
@@ -1,30 +1,52 @@
-v1 = int(input('Digite o valor 1: '))
-v2 = int(input('Digite o valor 2: '))
-op = int(input('''Digite a operação:
-1 - soma
-2 - subtração
-3 - multiplicação
-4 - divisão
-'''))
+print("=================================================================")
+print("=========== Projeto Calculadora Simples com Python ==============")
+print("=================================================================")
 
-if op == 1:
-    resultado = v1 + v2
-    print(f"Soma = {resultado}")
+while (True):
+    v1 = int(input('Digite o primeiro valor: '))
+	
+	v2 = int(input('Digite o primeiro valor: '))	
+	
+	op = int(input('''Qual operação você quer executar ? 
+               Escolha a opção:
+                1 - soma
+				2 - subtração
+				3 - multiplicação
+				4 - divisão
+				5 - potencia
+				6 - radiciação
+				7 - sair
+				'''))
+	
+	if op == 1:
+		resultado = v1 + v2
+		print(f"Soma = {resultado}")
 
-elif op == 2:
-    resultado = v1 - v2
-    print(f"Subtração = {resultado}")
+	elif op == 2:
+		resultado = v1 - v2
+		print(f"Subtração = {resultado}")
 
-elif op == 3:
-    resultado = v1 * v2
-    print(f"Multiplicação = {resultado}")
+	elif op == 3:
+		resultado = v1 * v2
+		print(f"Multiplicação = {resultado}")
 
-elif op == 4:
-    if v2 == 0:
-        print('Operação não permitida. Denominador não pode ser igual a zero.')
-    else:
-        resultado = v1 / v2
-        print(f"Divisão = {resultado}")
+	elif op == 4:
+		if v2 == 0:
+			print('Operação não permitida. Denominador não pode ser igual a zero.')
+			v2=int(input('[Re]Digite o valor de v2: ')) 
 
-else:
-    print('Opção inválida. Encerrando o programa.')
+		else:
+			resultado = v1 / v2
+			print(f"Divisão = {resultado}")
+
+	elif op ==5:
+    	resultado = 
+		print()
+	
+	elif op ==6:
+		resultado =
+		print()
+
+
+	else:
+		print('Opção inválida. Encerrando o programa.')
