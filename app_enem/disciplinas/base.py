eb44a2e0224110b22faee8d913d73e9260f606a1
Anderson <anderson.reis.barros@gmail.com>
2025-10-28 21:29:31 -0300

    Script react native

--- app_enem/disciplinas/base.py
+++
@@ -1,6 +0,0 @@
-class Disciplina:
-    def __init__(self, nome):
-        self.nome = nome
-
-    def descricao(self):
-        return f"Bem-vindo(a) à disciplina de {self.nome}!"
--- app_enem/disciplinas/quimica.py
+++
@@ -1,2 +0,0 @@
-class Quimica:
-    pass
